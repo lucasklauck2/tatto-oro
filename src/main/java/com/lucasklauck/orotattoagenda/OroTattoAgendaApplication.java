f2ee9df4c17b48cf012c667657425c6ba5776b54
Lucas Klauck <lucasklauck2@gmail.com>
2022-02-05 01:50:13 -0300

    primeiro commit

---
+++ src/main/java/com/lucasklauck/orotattoagenda/OroTattoAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasklauck.orotattoagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OroTattoAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OroTattoAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasklauck/orotattoagenda/OroTattoAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasklauck.orotattoagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OroTattoAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
