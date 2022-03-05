486dfc7a3bf6428dc53f9779eb1b035e87d7c2ac
Lucas Klauck <lucasklauck2@gmail.com>
2022-03-05 19:39:42 -0300

    Adicionado anotação faltante

--- src/main/java/com/lucasklauck/orotattoagenda/controller/DataInfoController.java
+++ src/main/java/com/lucasklauck/orotattoagenda/controller/DataInfoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class DataInfoController {
 
 	@CrossOrigin
 	@PostMapping
-	public ResponseEntity<Void> salvar(DataInfo dataInfo) {
+	public ResponseEntity<Void> salvar(@RequestBody DataInfo dataInfo) {
 		
 		dataInfoService.salvar(dataInfo);
 		
