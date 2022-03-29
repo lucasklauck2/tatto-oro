e4f38ce0fbb4177606ed8ca62cd834f6627ab712
Lucas Klauck <lucasklauck2@gmail.com>
2022-03-29 20:00:53 -0300

    Ajustado situação onde estava sendo criado mais de um DataInfo por data

--- src/main/java/com/lucasklauck/orotattoagenda/service/DataInfoService.java
+++ src/main/java/com/lucasklauck/orotattoagenda/service/DataInfoService.java
@@ -1,6 +1,7 @@
 package com.lucasklauck.orotattoagenda.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,27 @@ public class DataInfoService {
 	private DataInfoRepository dataInfoRepository;
 
 	public void salvar(DataInfo dataInfo) {
-		
+
+		if (dataInfo.getId() != null) {
+			
+			Optional<DataInfo> optDataInfo = dataInfoRepository.findById(dataInfo.getId());
+
+			if (optDataInfo.isEmpty()) {
+
+				DataInfo dataInfoExistente = optDataInfo.get();
+				dataInfoExistente.setCodigoCor(dataInfo.getCodigoCor());
+
+				dataInfoRepository.save(dataInfoExistente);
+				
+				return;
+			}
+		}
+
 		dataInfoRepository.save(dataInfo);
 	}
-	
+
 	public List<DataInfo> adquirirDatas() {
-		
+
 		return dataInfoRepository.findAll();
 	}
 }
